75eadb5b5a16a25e050c7201f2d2e59e4189cadd
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-07-21 03:49:49 -0700

    added challenge instructions and restructured code

--- includes/restaurant.js
+++ includes/restaurant.js
@@ -4,6 +4,28 @@ class Restaurant{
 		this.customerList = [];
 		this.orderQueue = [];
 		this.menu = menu;
+		this.maximumChefs = 3;
+		this.availableChefs = this.maximumChefs;
+		this.updateTimer = null;
+	}
+	openRestaraunt(){
+		//check if the updateTimer is not null.  if it isn't, the timer is running and needs to be stopped
+			//stop the timer by calling closeRestaurant
+		//use setInterval to start a timer that runs every 1 second, and calls checkQueue
+			//don't forget to bind the call or it won't work right
+			//don't forget to store the result of setInterval into your updateTimer for later
+	}
+	closeRestaraunt(){
+		//clearInterval on the updateTimer
+		//set updateTimer to null so it 
+	}
+	checkQueue(){
+		/*
+		check if an order is in the orderQueue
+			if there is, check if there is an available chef
+				if there is, reduce the availableChefs by 1
+				and call removeFromQueue to remove it from the pending orders
+		*/
 	}
 	addCustomer( name, hungerLevel, cash ){
 		var customer = new Customer(name, hungerLevel, cash, this);
@@ -16,7 +38,7 @@ class Restaurant{
 		customer.requestOrder( this.menu );
 	}
 	placeOrder( itemDesired, customer){
-		console.log(`${itemDesired.name} is cooking for ${customer.name}!` );
+		console.log(`${customer.name} has placed an order for ${itemDesired.name}!` );
 		this.addToQueue(itemDesired, customer);
 	}
 	addToQueue( item, customer){
@@ -25,12 +47,18 @@ class Restaurant{
 		var randomTime =  Math.random() * (item.cookingTime.max - item.cookingTime.min )  + item.cookingTime.min;
 		randomTime *= 1000;
 		this.orderQueue.push( orderObject );
-		setTimeout( this.removeFromQueue.bind(this,orderObject), randomTime )
-		
+		console.log(`*********${completedItem.item.name} has been added to the queue for  ${completedItem.customer.name}!`);
+	}
+	startCookingOrder(order){
+		console.log(`*********starting on order of ${completedItem.item.name} for ${completedItem.customer.name}!`);
+		setTimeout( this.finishCookingOrder.bind(this,orderObject), randomTime );
+	}
+	finishCookingOrder(order){
+		console.log(`*********${completedItem.item.name} is ready for ${completedItem.customer.name}!`);
+		//increase the availableChefs count by 1
 	}
 	removeFromQueue( completedItem ){
 		var itemIndex = this.orderQueue.indexOf( completedItem );
-		console.log(`*********${completedItem.item.name} is ready for ${completedItem.customer.name}!`);
 		this.orderQueue.splice(itemIndex, 1);
 	}
 }
